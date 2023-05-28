1baceab5da3748b3797cf34252c5f7044ec6072d
Daniel Lezama <daniel@maidepot.com>
2023-05-27 22:58:43 -0500

    removed files

--- gym_environments/gym_environments/envs/puzzles/v0/hero/game/settings.py
+++ gym_environments/gym_environments/envs/puzzles/v0/hero/game/settings.py
@@ -22,9 +22,6 @@ ENVIRONMENT = BASE_DIR / "env.txt"
 # Graphics
 GAME_TEXTURES = {
     "background": pygame.image.load(BASE_DIR / "graphics" / "background.png"),
-    "tiles": pygame.image.load(BASE_DIR / "graphics" / "sheet.png"),
-    "main_character": pygame.image.load(BASE_DIR / "graphics" / "main_character.png"),
-    "statues": pygame.image.load(BASE_DIR / "graphics" / "statues.png"),
     "floor": pygame.image.load(BASE_DIR / "graphics" / "floor.png"),
     "hero": pygame.image.load(BASE_DIR / "graphics" / "hero.png"),
     "item": pygame.image.load(BASE_DIR / "graphics" / "item.png"),
@@ -43,11 +40,6 @@ SOUNDS = {
 
 # Frames
 GAME_FRAMES = {
-    "tiles": generate_frames(GAME_TEXTURES["tiles"], TILE_SIZE, TILE_SIZE),
-    "main_character": generate_frames(
-        GAME_TEXTURES["main_character"], PLAYER_WIDTH, PLAYER_HEIGHT
-    ),
-    "statues": generate_frames(GAME_TEXTURES["statues"], BOSS_WIDTH, BOSS_HEIGHT),
     "floor": generate_frames(GAME_TEXTURES["floor"], TILE_SIZE, TILE_SIZE),
     "hero": generate_frames(GAME_TEXTURES["hero"], TILE_SIZE, TILE_SIZE),
     "item": generate_frames(GAME_TEXTURES["item"], TILE_SIZE, TILE_SIZE),
